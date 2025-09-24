d1d0d82354bf3f3a8bbf7ee011870f201464f706
rok <rexblanegamer@gmail.com>
2025-09-23 22:33:37 -0300

    refactor: simplify RedisDatabase by using RedisConnector for resource management

--- core-minecraft/src/main/java/net/warcane/lugin/core/minecraft/punish/core/database/redis/RedisDatabase.java
+++ core-minecraft/src/main/java/net/warcane/lugin/core/minecraft/punish/core/database/redis/RedisDatabase.java
@@ -1,6 +1,7 @@
 package net.warcane.lugin.core.minecraft.punish.core.database.redis;
 
 import lombok.Getter;
+import net.warcane.lugin.core.database.RedisConnector;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import redis.clients.jedis.Jedis;
@@ -16,33 +17,23 @@ public class RedisDatabase {
 
     private static RedisDatabase instance;
 
-    protected JedisPool pool;
-
-    private RedisDatabase(String host) {
-        pool = new JedisPool(host);
-    }
-
     public void subscribe(Plugin plugin, JedisPubSub jedisPubSub, String... channels) {
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            try (Jedis jedis = pool.getResource()) {
+            RedisConnector.getInstance().useJedis(jedis -> {
                 jedis.subscribe(jedisPubSub, channels);
-            } catch (Exception e) {
-                plugin.getLogger().severe("Failed to subscribe to Redis channels: " + e.getMessage());
-            }
+            });
         });
     }
 
     public void publish(PubSubMessage message) {
-        try (Jedis jedis = pool.getResource()) {
+        RedisConnector.getInstance().useJedis(jedis -> {
             jedis.publish(message.getChannel(), message.serialize());
-        } catch (Exception e) {
-            Bukkit.getLogger().severe("Failed to publish message to Redis channel: " + e.getMessage());
-        }
+        });
     }
 
     public static void init() {
         if (instance == null) {
-            instance = new RedisDatabase(System.getProperty("REDIS_URL"));
+            instance = new RedisDatabase();
             return;
         }
         throw new IllegalStateException("RedisDatabase is already initialized.");
