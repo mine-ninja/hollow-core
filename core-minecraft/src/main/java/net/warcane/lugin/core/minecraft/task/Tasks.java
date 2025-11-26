408fc4e85d9aa804097a8ce5942ac78e511caddc
Matheus Barreto <69403805+mtbarr@users.noreply.github.com>
2025-11-25 23:58:19 -0300

    feat: validate online status of player before processing payment command

--- core-minecraft/src/main/java/net/warcane/lugin/core/minecraft/task/Tasks.java
+++ core-minecraft/src/main/java/net/warcane/lugin/core/minecraft/task/Tasks.java
@@ -6,109 +6,74 @@ import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
-import java.util.function.Consumer;
 
 public class Tasks {
 
     private static Plugin plugin;
     private static FoliaLib foliaLib;
 
-    // todo achar uma forma de fazer isso de uma forma menos nojenta.
-    private static Plugin findFirstPlugin() {
-        if (plugin != null) {
-            return plugin;
+    private static void initialize() {
+        if (plugin == null) {
+            plugin = Arrays.stream(Bukkit.getPluginManager().getPlugins())
+              .filter(Plugin::isEnabled)
+              .findFirst()
+              .orElseThrow(() -> new IllegalStateException("No enabled plugin found."));
+
+            foliaLib = new FoliaLib(plugin);
         }
-
-        final var foundPlugin = plugin = Arrays.stream(Bukkit.getPluginManager().getPlugins())
-            .filter(Plugin::isEnabled)
-            .findFirst()
-            .orElseThrow(() -> new IllegalStateException("No enabled plugin found."));
-
-        if (foliaLib == null) {
-            foliaLib = new FoliaLib(foundPlugin);
-        }
-
-        return plugin;
     }
-
-    private static boolean isFolia() {
-        return foliaLib != null && foliaLib.isFolia();
-    }
-
 
     public static void runSyncLater(@NotNull Runnable runnable, long delay) {
-        usePlugin(plugin -> {
-            if (!isFolia()) {
-                Bukkit.getScheduler().runTaskLater(plugin, runnable, delay);
-                return;
-            }
-
+        initialize();
+        if (foliaLib.isFolia()) {
             foliaLib.getScheduler().runLater(runnable, delay);
-        });
+        } else {
+            Bukkit.getScheduler().runTaskLater(plugin, runnable, delay);
+        }
     }
 
-
     public static void runSync(@NotNull Runnable runnable) {
-        usePlugin(plugin -> {
-            if (!isFolia()) {
-                Bukkit.getScheduler().runTask(plugin, runnable);
-            } else {
-                foliaLib.getScheduler().runNextTick(task -> runnable.run());
-            }
-        });
+        initialize();
+        if (foliaLib.isFolia()) {
+            foliaLib.getScheduler().runNextTick(task -> runnable.run());
+        } else {
+            Bukkit.getScheduler().runTask(plugin, runnable);
+        }
     }
 
     public static void runSyncRepeating(@NotNull Runnable runnable, long delay, long period) {
-        usePlugin(plugin -> {
-            if (!isFolia()) {
-                Bukkit.getScheduler().runTaskTimer(plugin, runnable, delay, period);
-                return;
-            }
-
+        initialize();
+        if (foliaLib.isFolia()) {
             foliaLib.getScheduler().runTimer(runnable, delay, period);
-        });
+        } else {
+            Bukkit.getScheduler().runTaskTimer(plugin, runnable, delay, period);
+        }
     }
 
     public static void runAsync(@NotNull Runnable runnable) {
-        usePlugin(plugin -> {
-            if (!isFolia()) {
-                Bukkit.getScheduler().runTaskAsynchronously(plugin, runnable);
-                return;
-            }
-
-
+        initialize();
+        if (foliaLib.isFolia()) {
             foliaLib.getScheduler().runAsync(task -> runnable.run());
-        });
+        } else {
+            Bukkit.getScheduler().runTaskAsynchronously(plugin, runnable);
+        }
     }
 
     public static void runAsyncLater(@NotNull Runnable runnable, long delay) {
-        usePlugin(plugin -> {
-            if (!isFolia()) {
-                Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, runnable, delay);
-                return;
-            }
-
+        initialize();
+        if (foliaLib.isFolia()) {
             foliaLib.getScheduler().runLaterAsync(runnable, delay);
-        });
+        } else {
+            Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, runnable, delay);
+        }
     }
 
     public static void runAsyncRepeating(@NotNull Runnable runnable, long delay, long period) {
-        usePlugin(plugin -> {
-            if (!isFolia()) {
-                Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, runnable, delay, period);
-                return;
-            }
-
+        initialize();
+        if (foliaLib.isFolia()) {
             foliaLib.getScheduler().runTimerAsync(runnable, delay, period);
-        });
-    }
-
-    public static void usePlugin(@NotNull Consumer<Plugin> consumer) {
-        Plugin plugin = findFirstPlugin();
-        if (plugin != null) {
-            consumer.accept(plugin);
         } else {
-            throw new IllegalStateException("No enabled plugin found to use.");
+            Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, runnable, delay, period);
         }
     }
 }
